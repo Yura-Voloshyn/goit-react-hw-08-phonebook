a4a0c4ecb47c5a79ed195b45a22e7b2f2b65164a
Yura-Voloshyn <94964569+Yura-Voloshyn@users.noreply.github.com>
2022-09-20 19:38:34 +0000

    Deploying to gh-pages from @ Yura-Voloshyn/goit-react-hw-08-phonebook@e6475aa8fab06a8b6166c32e74188be7b344fbea 🚀

---
+++ static/js/544.813844a2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_phonebook=self.webpackChunkreact_phonebook||[]).push([[544],{9544:function(e,n,o){o.r(n);var c=o(184);n.default=function(){return(0,c.jsx)("main",{children:(0,c.jsx)("h1",{children:"Welcome to home page"})})}}}]);
+//# sourceMappingURL=544.813844a2.chunk.js.map
